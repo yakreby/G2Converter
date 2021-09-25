a603e3cc1132bd59a11c9e9aeb12bb9b4365cbd8
Berkay Yıldırım <55652632+yakreby@users.noreply.github.com>
2021-09-25 16:50:21 +0300

    Proje dosyası ekle.

---
+++ G2Converter/G2Converter.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace G2Converter
+{
+    partial class G2Converter
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(G2Converter));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.ConvertBtn = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "XML Dosyasını Buraya Sürükleyip Bırakınız";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listBox2);
+            this.panel2.Location = new System.Drawing.Point(494, 65);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(286, 306);
+            this.panel2.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(9, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(274, 292);
+            this.listBox2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Location = new System.Drawing.Point(33, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 306);
+            this.panel1.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(6, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(274, 292);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox1_DragEnter);
+            // 
+            // ConvertBtn
+            // 
+            this.ConvertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ConvertBtn.Location = new System.Drawing.Point(319, 171);
+            this.ConvertBtn.Name = "ConvertBtn";
+            this.ConvertBtn.Size = new System.Drawing.Size(169, 50);
+            this.ConvertBtn.TabIndex = 4;
+            this.ConvertBtn.Text = "Çevir";
+            this.ConvertBtn.UseVisualStyleBackColor = true;
+            this.ConvertBtn.Click += new System.EventHandler(this.ConvertBtn_Click);
+            // 
+            // G2Converter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(834, 434);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ConvertBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "G2Converter";
+            this.Text = "G2Converter";
+            this.Load += new System.EventHandler(this.G2Converter_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button ConvertBtn;
+    }
+}
+
---
+++ G2Converter/G2Converter.cs
@@ -0,0 +1,122 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace G2Converter
+{
+    public partial class G2Converter : Form
+    {
+        public G2Converter()
+        {
+            InitializeComponent();
+        }
+
+        //Drag Enter
+        void ListBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        //Drag Drop
+        void ListBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                //Declare File Type
+                if (!file.ToString().Contains("xml") && !file.ToString().Contains("html"))
+                {
+                    MessageBox.Show("Lütfen Geçerli bir dosya yükleyiniz.");
+                }
+                else
+                {
+                    listBox1.Items.Add(file.ToString());
+                }
+            }
+            ConvertBtn.Enabled = true;
+        }
+
+        private void ConvertBtn_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                Convert();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen sol taraftaki listeden çevrilecek olan dosyayı seçiniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        //Convert Operation
+        private void Convert()
+        {
+            string fileName = listBox1.SelectedItem.ToString();
+            XDocument xmlFile = XDocument.Load(fileName);
+            //Result File
+            Random rnd = new Random();
+            var outputFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), $"Sonuc{rnd.Next()}.pdf");
+
+            using (var fs = new FileStream(outputFile, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                using (var doc = new Document())
+                {
+                    using (var writer = PdfWriter.GetInstance(doc, fs))
+                    {
+                        doc.Open();
+
+                        //Column count
+                        var columnCount = xmlFile.Root.Elements().First().Nodes().Count();
+
+                        //Create a table with one column for every child node of elements
+                        var t = new PdfPTable(columnCount)
+                        {
+                            //Flag that the first row should be repeated on each page break
+                            HeaderRows = 1
+                        };
+
+                        //Loop through the first item to output column headers
+                        foreach (var N in xmlFile.Root.Elements().First().Elements())
+                        {
+                            t.AddCell(N.Name.ToString());
+                        }
+
+                        //Loop through each CD row (this is so we can call complete later on)
+                        foreach (var CD in xmlFile.Root.Elements())
+                        {
+                            //Loop through each child of the current CD. Limit the number of children to our initial count just in case there are extra nodes.
+                            foreach (var N in CD.Elements().Take(columnCount))
+                            {
+                                t.AddCell(N.Value);
+                            }
+                            //Just in case any rows have too few cells fill in any blanks
+                            t.CompleteRow();
+                        }
+                        //Add the table to the document
+                        doc.Add(t);
+                        doc.Close();
+                        MessageBox.Show("Döküman Başarıyla Çevrildi", "Çevirme Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+        private void G2Converter_Load(object sender, EventArgs e)
+        {
+            //Disable Convert Button
+            if (listBox1.Items.Count == 0)
+            {
+                ConvertBtn.Enabled = false;
+            }
+        }
+    }
+}
